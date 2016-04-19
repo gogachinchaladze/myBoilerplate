3d199f6bfc16c69a4e4872ee5b64de6fade4bc9a
Goga Chinchaladze <goga.chinchaladze@gmail.com>
2016-04-19 12:37:32 +0400

    Added Capitalize Function

--- js/lib/my-lib.js
+++ js/lib/my-lib.js
@@ -198,7 +198,9 @@ MyLib.prototype.validateEmail = function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 };
-
+MyLib.prototype.capitalize = function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 //Helper that allows to skip the NEW keyword
 function G(selector){
   return new MyLib(selector);
